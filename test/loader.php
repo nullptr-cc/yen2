da02ac7b9ef93aeffdec7141b23d24402e977c5f
Egor V. Derevyankin <egor@nullptr.cc>
2015-08-03 09:44:26 +0300

    + autoloading for YenTest namespace

--- test/loader.php
+++ test/loader.php
@@ -2,7 +2,9 @@
 
 spl_autoload_register(function($classname) {
     if (strpos($classname, 'Yen\\') === 0) {
-        include_once __DIR__ . '/../src/' . str_replace('\\', '/', $classname) . '.php';
+        @include_once __DIR__ . '/../src/' . str_replace('\\', '/', $classname) . '.php';
+    } elseif (strpos($classname, 'YenTest\\') === 0) {
+        @include_once __DIR__ . '/' . str_replace('\\', '/', $classname) . '.php';
     } else {
         return false;
     };
