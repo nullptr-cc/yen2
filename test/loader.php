6fb1cd55baf9975f08837b2b0fcda3ee0457ca7e
Egor V. Derevyankin <egor@nullptr.cc>
2016-02-14 14:28:57 +0300

    - error suppression (@) from loader in tests

--- test/loader.php
+++ test/loader.php
@@ -1,12 +1,18 @@
 <?php
 
 spl_autoload_register(function ($classname) {
+    $include = function ($file) {
+        if (!file_exists($file)) {
+            return false;
+        };
+        include_once $file;
+    };
     if (strpos($classname, 'Yen\\') === 0) {
-        @include_once __DIR__ . '/../src/' . str_replace('\\', '/', $classname) . '.php';
+        $include(__DIR__ . '/../src/' . str_replace('\\', '/', $classname) . '.php');
     } elseif (strpos($classname, 'YenTest\\') === 0) {
-        @include_once __DIR__ . '/' . str_replace('\\', '/', $classname) . '.php';
+        $include(__DIR__ . '/' . str_replace('\\', '/', $classname) . '.php');
     } elseif (strpos($classname, 'YenMock\\') === 0) {
-        @include_once __DIR__ . '/' . str_replace('\\', '/', $classname) . '.php';
+        $include(__DIR__ . '/' . str_replace('\\', '/', $classname) . '.php');
     } else {
         return false;
     };
