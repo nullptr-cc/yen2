ba5a5ac8555b9b719a854aabc2c86a96f9c1ba54
Egor V. Derevyankin <egor@nullptr.cc>
2016-03-07 13:31:02 +0300

    * Core   - unnecessary code   * minor changes for request processing in FrontController

--- src/Yen/Core/FrontController.php
+++ src/Yen/Core/FrontController.php
@@ -2,43 +2,29 @@
 
 namespace Yen\Core;
 
-use Yen\Http;
-use Yen\Router;
-use Yen\Handler;
-use Yen\View;
+use Yen\Http\Contract\IServerRequest;
+use Yen\Router\Contract\IRouter;
+use Yen\Handler\Contract\IHandlerRegistry;
 
 class FrontController
 {
     protected $router;
     protected $handler_registry;
-    protected $view_registry;
 
     public function __construct(
-        Router\Contract\IRouter $router,
-        Handler\Contract\IHandlerRegistry $handler_registry,
-        View\Contract\IViewRegistry $view_registry
+        IRouter $router,
+        IHandlerRegistry $handler_registry
     ) {
         $this->router = $router;
         $this->handler_registry = $handler_registry;
-        $this->view_registry = $view_registry;
     }
 
-    public static function createFromDC(Contract\IDependencyContainer $dc)
-    {
-        return new self(
-            $dc->getRouter(),
-            $dc->getHandlerRegistry(),
-            $dc->getViewRegistry()
-        );
-    }
-
-    public function processRequest(Http\Contract\IServerRequest $request)
+    public function processRequest(IServerRequest $request)
     {
         $route = $this->router->route($request->getUri()->getPath());
         $handler = $this->handler_registry->getHandler($route->entry());
         $response = $handler->handle($request->withJoinedQueryParams($route->arguments()));
-        $view = $this->view_registry->getView($route->entry());
 
-        return $view->present($request->getMethod(), $response);
+        return $response;
     }
 }
