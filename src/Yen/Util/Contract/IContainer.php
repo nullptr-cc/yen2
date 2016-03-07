57b9e0d5aa5a88d631de39005c13e4a70cfd132a
Egor V. Derevyankin <egor@nullptr.cc>
2016-03-07 13:13:27 +0300

    - too common and unnecessary Utils

--- src/Yen/Util/Contract/IContainer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Yen\Util\Contract;
-
-interface IContainer
-{
-    public function has($name);
-    public function get($name);
-}
--- src/Yen/Util/Contract/IFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Yen\Util\Contract;
-
-interface IFactory
-{
-    public function make($name);
-    public function canMake($name);
-}
