e658c1b13a13bd1817ccd6b42d967e19a0eb85eb
Egor V. Derevyankin <egor@nullptr.cc>
2016-03-23 16:04:17 +0300

    * constants for Http

---
+++ src/Yen/Http/Contract/IRequest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Yen\Http\Contract;
+
+interface IRequest
+{
+    const METHOD_GET = 'GET';
+    const METHOD_POST = 'POST';
+}
--- src/Yen/Http/Contract/IResponse.php
+++ src/Yen/Http/Contract/IResponse.php
@@ -4,6 +4,18 @@ namespace Yen\Http\Contract;
 
 interface IResponse
 {
+    const STATUS_OK = 200;
+
+    const STATUS_MOVED_PERMANENTLY = 301;
+    const STATUS_MOVED_TEMPORARY = 302;
+
+    const STATUS_BAD_REQUEST = 400;
+    const STATUS_FORBIDDEN = 403;
+    const STATUS_NOT_FOUND = 404;
+    const STATUS_METHOD_NOT_ALLOWED = 405;
+
+    const STATUS_INTERNAL_ERROR = 500;
+
     public function getStatusCode();
     public function getBody();
     public function getHeaders();
